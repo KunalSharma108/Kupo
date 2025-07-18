069407e99347d1cddc0bd3844058a83232013d7b
kunalsharma108 <kunalsharma0422@gmail.com>
2025-07-18 16:34:18 +0530

    The background type is done. this thing gonna suck the life out of me fr fr

--- src/renderer/src/components/Dashboard/components/styleDialog.tsx
+++ src/renderer/src/components/Dashboard/components/styleDialog.tsx
@@ -46,13 +46,7 @@ export const StyleDialog: React.FC<StyleDialogProps> = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [canConfirm, setCanConfirm] = useState(false);
-  const [color, setColor] = useState<string>(value[0] === '#' ? value : '#000000ff')
-  const hasShownImageDialog = useRef(false)
-  const [gradientValue, setGradientValue] = useState<string[]>(
-    typeof value === 'string' && value.split(' ').length !== 2 ? value.split(' ') : ['#000000ff', '#000000ff', '#fff000', 'to-right']
-  )
-  const [gradientDirection, setGradientDirection] = useState<string>(gradientValue[gradientValue.length - 1])
-  const [gradientDirectionOpen, setGradientDirectionOpen] = useState<boolean>(false);
+  const [color, setColor] = useState<string>(value[0] === '#' ? value : '#000000ff');
 
   useEffect(() => {
     if (inputValue != value) {
@@ -114,24 +108,6 @@ export const StyleDialog: React.FC<StyleDialogProps> = ({
       onClose();
     }, 300);
   };
-
-  const showSelectImage = async () => {
-    const response = await selectImage()
-
-    if (!response.canceled && response.filePaths.length === 1) {
-      setInputValue(response.filePaths);
-    } else {
-      alert('An error occured');
-      handleClose();
-    }
-  }
-
-  useEffect(() => {
-    if (subType.toLowerCase() === 'image' && !hasShownImageDialog.current) {
-      hasShownImageDialog.current = true;
-      showSelectImage();
-    }
-  }, [subType])
 
   function renderInput(type: string, subType: string) {
     if (type.toLowerCase() === 'background') {
@@ -238,17 +214,59 @@ export const StyleDialog: React.FC<StyleDialogProps> = ({
           </div>
         );
       } else if (subType.toLowerCase() === 'image') {
+        const hasShownImageDialog = useRef(false);
+        const [imagePath, setImagePath] = useState<string | false>(value)
+
+        const showSelectImage = async () => {
+          const response = await selectImage()
+
+          if (!response.canceled && response.filePaths.length === 1) {
+            console.log(response.filePaths[0])
+            setInputValue(response.filePaths[0]);
+            setImagePath(response.filePaths[0]);
+          }
+        }
+
+        useEffect(() => {
+          if (subType.toLowerCase() === 'image' && !hasShownImageDialog.current && imagePath === false) {
+            hasShownImageDialog.current = true;
+            showSelectImage();
+          }
+        }, [subType])
+
         return (
-          <input
-            ref={inputRef}
-            className="style-dialog-input"
-            value={inputValue !== false ? inputValue : 'Waiting for an Image to be selected'}
-            onChange={(e) => setInputValue(e.target.value)}
-            autoFocus={false}
-            disabled={true}
-          />
+          <div
+            className="style-input-wrapper"
+          >
+            <p className="gradient-direction-heading">Image: </p>
+
+            <input
+              ref={inputRef}
+              className="style-dialog-input"
+              value={imagePath !== false ? imagePath : 'Waiting for an Image to be selected'}
+              onChange={(e) => setInputValue(e.target.value)}
+              autoFocus={false}
+              disabled={true}
+              style={{ pointerEvents: 'none' }}
+            />
+
+            {imagePath && (
+              <button className="change-image-btn" onClick={showSelectImage}>
+                Change
+              </button>
+            )}
+          </div>
         )
       } else if (subType.toLowerCase() === 'gradient') {
+
+        const gradientValue =
+          typeof value === 'string' && subType.toLowerCase() === 'gradient' && value.split(' ').length !== 2
+            ? value.split(' ')
+            : ['#000000ff', '#000000ff', '#fff000', 'to-right']
+
+        const [gradientDirection, setGradientDirection] = useState<string>(gradientValue[gradientValue.length - 1])
+        const [gradientDirectionOpen, setGradientDirectionOpen] = useState<boolean>(false);
+
         const [gradientColorValues, setGradientColorValues] = useState<string[]>(
           gradientValue.filter((_, idx) => idx !== gradientValue.length - 1)
         )
@@ -363,7 +381,7 @@ export const StyleDialog: React.FC<StyleDialogProps> = ({
                       <div className="gradient-color">{val}</div>
                       <div className="color-circle" style={{ backgroundColor: val }}></div>
                     </div>
-                    {gradientValue.length > 2 && idx !== 0 && idx !== 1 && (
+                    {gradientColorValues.length > 2 && idx !== 0 && idx !== 1 && (
                       <div
                         className="gradient-minus"
                         onClick={() => handleRemoveColor(idx)}
@@ -392,16 +410,223 @@ export const StyleDialog: React.FC<StyleDialogProps> = ({
             </div>
           </>
         )
+      } else if (subType.toLowerCase() === 'image + gradient') {
+
+        const updateInputValue = (filePath: string | false, gradientColorArray: string[], direction: string) => {
+          if (!filePath) return;
+          let text = filePath + ',' + gradientColorArray.join(' ') + ' ' + direction
+          console.log(text)
+          setInputValue(text);
+        }
+
+        const hasShownImageDialog = useRef(false);
+
+        const imageGradientColors =
+          typeof value === 'string' && subType.toLowerCase() === 'image + gradient' && value.split(',').length > 1 ?
+            value.split(',')[1].split(' ') :
+            ['#fff000', '#4c21a9ff', 'to-right']
+
+        const [imagePath, setImagePath] = useState<string | false>(
+          typeof value === 'string' && subType.toLowerCase() === 'image + gradient' && value.split(',').length > 1 ?
+            value.split(',')[0] :
+            false
+        )
+
+        const [imageGradientColorValues, setImageGradientColorValues] = useState<string[]>(
+          imageGradientColors.filter((_, idx) => idx !== imageGradientColors.length - 1)
+        )
+
+        const [imageGradientDirection, setImageGradientDirection] = useState<string>(
+          imageGradientColors[imageGradientColors.length - 1]
+        )
+
+        const [imageGradientDirectionOpen, setImageGradientDirectionOpen] = useState<boolean>(false);
+
+        const [selectedImageGradient, setSelectedImageGradient] = useState<number>(0);
+        const [selectedImageGradientValue, setSelectedImageGradientValue] = useState<string>(
+          imageGradientColorValues[selectedImageGradient]
+        );
+
+        const showSelectImage = async () => {
+          const response = await selectImage();
+
+          if (!response.canceled && response.filePaths.length === 1) {
+            setImagePath(response.filePaths)
+            updateInputValue(response.filePaths, imageGradientColorValues, imageGradientDirection);
+          }
+        }
+
+        useEffect(() => {
+          if (subType.toLowerCase() === 'image + gradient' && !hasShownImageDialog.current && imagePath === false) {
+            hasShownImageDialog.current = true;
+            showSelectImage();
+          }
+        }, [subType])
+
+        const handleColorChange = (newColor: string) => {
+          setSelectedImageGradientValue(newColor);
+          const updatedGradient = [...imageGradientColorValues];
+          updatedGradient[selectedImageGradient] = newColor;
+          setImageGradientColorValues(updatedGradient);
+          updateInputValue(imagePath, updatedGradient, imageGradientDirection)
+        };
+
+        const handleSelect = (idx: number) => {
+          setSelectedImageGradient(idx);
+          setSelectedImageGradientValue(imageGradientColorValues[idx]);
+        };
+
+        const handleAddColor = () => {
+          const newGradient = [...imageGradientColorValues, "#ffffff"];
+          setImageGradientColorValues(newGradient);
+          setSelectedImageGradient(newGradient.length - 1);
+          setSelectedImageGradientValue("#ffffff");
+          updateInputValue(imagePath, newGradient, imageGradientDirection);
+        };
+
+        const handleRemoveColor = (idx: number) => {
+          console.log('got')
+          if (imageGradientColorValues.length <= 2) return;
+          const newGradient = imageGradientColorValues.filter((_, i) => i !== idx);
+          setImageGradientColorValues(newGradient);
+          updateInputValue(imagePath, newGradient, imageGradientDirection);
+
+          if (selectedImageGradient >= newGradient.length) {
+            setSelectedImageGradient(newGradient.length - 1);
+            setSelectedImageGradientValue(newGradient[newGradient.length - 1]);
+          } else {
+            setSelectedImageGradientValue(newGradient[selectedImageGradient]);
+          }
+        };
+
+        const getArrowRotation = (direction: string) => {
+          switch (direction) {
+            case "to-top": return "rotate(0deg)";
+            case "to-top-right": return "rotate(45deg)";
+            case "to-right": return "rotate(90deg)";
+            case "to-bottom-right": return "rotate(135deg)";
+            case "to-bottom": return "rotate(180deg)";
+            case "to-bottom-left": return "rotate(225deg)";
+            case "to-left": return "rotate(270deg)";
+            case "to-top-left": return "rotate(315deg)";
+            default: return "rotate(0deg)";
+          }
+        };
+
+        return (
+          <>
+            <div
+              className="style-input-wrapper"
+            >
+              <p className="gradient-direction-heading">Image: </p>
+
+              <input
+                ref={inputRef}
+                className="style-dialog-input"
+                value={imagePath !== false ? imagePath : 'Waiting for an Image to be selected'}
+                onChange={(e) => setInputValue(e.target.value)}
+                autoFocus={false}
+                disabled={true}
+                style={{ pointerEvents: 'none' }}
+              />
+
+              {imagePath && (
+                <button className="change-image-btn" onClick={showSelectImage}>
+                  Change
+                </button>
+              )}
+            </div>
+
+            <div className="gradient-direction-wrapper">
+
+              <p className='gradient-direction-heading'>Direction: </p>
+              <div className="style-color-options-dropdown">
+                <div
+                  className="dropdown-selected"
+                  onClick={() => setImageGradientDirectionOpen(!imageGradientDirectionOpen)}
+                >
+                  <span className="selected-label">
+                    {imageGradientDirection || "Select a type"}
+                  </span>
+
+                  <FontAwesomeIcon icon={faChevronDown} className="dropdown-arrow" />
+                </div>
+
+                {imageGradientDirectionOpen && (
+                  <div className="dropdown-options">
+                    {gradientDirectionValue.map((option) => (
+                      <div
+                        key={option}
+                        className={`dropdown-option ${imageGradientDirection === option ? "selected" : ""}`}
+                        onClick={() => {
+                          setImageGradientDirection(option);
+                          setImageGradientDirectionOpen(false);
+                          updateInputValue(imagePath, imageGradientColorValues, option);
+                        }}
+                      >
+                        {option}
+                        <FontAwesomeIcon
+                          icon={faArrowUp}
+                          style={{ transform: getArrowRotation(option), marginLeft: "8px" }}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="gradient-color-wrapper">
+              <div className="gradient-color-array">
+                {imageGradientColorValues.map((val, idx) => (
+                  <div className="gradient-color-row" key={idx}>
+                    <div
+                      className={`gradient-color-holder ${idx === selectedImageGradient ? 'selected' : ''}`}
+                      onClick={() => handleSelect(idx)}
+                    >
+                      <div className="gradient-color">{val}</div>
+                      <div className="color-circle" style={{ backgroundColor: val }}></div>
+                    </div>
+                    {imageGradientColorValues.length > 2 && idx !== 0 && idx !== 1 && (
+                      <div
+                        className="gradient-minus"
+                        onClick={() => handleRemoveColor(idx)}
+                      >
+                        <FontAwesomeIcon icon={faMinus} />
+                      </div>
+                    )}
+                  </div>
+                ))}
+
+                <div className="gradient-plus" onClick={handleAddColor}>
+                  <FontAwesomeIcon icon={faPlus} />
+                </div>
+              </div>
+
+              <div className="gradient-color-picker-wrapper">
+                <HexColorPicker color={selectedImageGradientValue} onChange={handleColorChange} />
+                <input
+                  className='color-picker-input'
+                  ref={inputRef}
+                  type="text"
+                  value={selectedImageGradientValue}
+                  onChange={(e) => handleColorChange(e.target.value)}
+                />
+              </div>
+            </div>
+          </>
+        )
+
       } else {
         return (
-          <input
-            ref={inputRef}
-            className="style-dialog-input"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            autoFocus={true}
-          />
-        )
+          <div className="style-dialog-error">
+            <h4>⚠️ Technical Error</h4>
+            <p>
+              Something went wrong while loading this input field.
+              <br />
+              Try restarting the program if this keeps happening.
+            </p>
+          </div>
+        );
       }
 
     } else {
